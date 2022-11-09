19830c356b0ccb48f51f427cca0ee216028ffc56
LordUukon <bodiburris@gmail.com>
2022-11-09 10:04:31 -0600

    Interaction Script Began

--- TheForeFathers_1.0.1/Assets/Scripts/PlayerInteraction.cs
+++ TheForeFathers_1.0.1/Assets/Scripts/PlayerInteraction.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteract : MonoBehaviour
+public class PlayerInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float interactionDistance;
+
+    public TMPro.TextMeshProUGUI interactionText;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
